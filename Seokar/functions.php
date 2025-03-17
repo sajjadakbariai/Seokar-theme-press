bf177b57653eb67e37ba6970d3d7ba0b8b1bc845
sajjad akbari <seokarid@gmail.com>
2025-03-17 16:46:13 +0330

    Update functions.php

--- Seokar/functions.php
+++ Seokar/functions.php
@@ -28,3 +28,13 @@ function seokar_register_menus() {
     ]);
 }
 add_action('after_setup_theme', 'seokar_register_menus');
+// بارگذاری استایل‌ها و اسکریپت‌ها
+function seokar_enqueue_styles() {
+    wp_enqueue_style('seokar-reset', get_template_directory_uri() . '/assets/css/reset.css', [], '1.0.0');
+    wp_enqueue_style('seokar-header', get_template_directory_uri() . '/assets/css/header.css', [], '1.0.0');
+    wp_enqueue_style('seokar-footer', get_template_directory_uri() . '/assets/css/footer.css', [], '1.0.0');
+    wp_enqueue_style('seokar-main', get_template_directory_uri() . '/assets/css/main.css', [], '1.0.0');
+    wp_enqueue_style('seokar-responsive', get_template_directory_uri() . '/assets/css/responsive.css', [], '1.0.0');
+    wp_enqueue_style('seokar-style', get_stylesheet_uri(), [], '1.0.0');
+}
+add_action('wp_enqueue_scripts', 'seokar_enqueue_styles');
