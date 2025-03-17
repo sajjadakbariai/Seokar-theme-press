046a8e1846e83ee02f14efca260edbafc78424c8
sajjad akbari <seokarid@gmail.com>
2025-03-17 16:10:32 +0330

    Update functions.php

--- Seokar/functions.php
+++ Seokar/functions.php
@@ -14,3 +14,11 @@ require_once get_template_directory() . '/inc/autoload.php';
 Seokar\Enqueue::register();
 Seokar\Menus::register();
 Seokar\Security::apply();
+
+/**
+ * بارگذاری استایل اصلی قالب
+ */
+function seokar_enqueue_styles() {
+    wp_enqueue_style('seokar-style', get_template_directory_uri() . '/assets/style.css', [], '1.0.0');
+}
+add_action('wp_enqueue_scripts', 'seokar_enqueue_styles');
