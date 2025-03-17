4a1febe1e14b89262073d3ab8d21b5a11b190792
sajjad akbari <seokarid@gmail.com>
2025-03-17 16:42:19 +0330

    Update footer.php

---
+++ Seokar/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Footer Template
+ *
+ * @package Seokar
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
+<footer class="footer">
+    <div class="container">
+        <p>&copy; <?php echo date( 'Y' ); ?> - همه حقوق محفوظ است. طراحی شده توسط <a href="https://sajjadakbari.ir">سجاد اکبری</a></p>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
