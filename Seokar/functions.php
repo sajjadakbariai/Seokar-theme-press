d9297fc36b3ec5e132fd7ef850f67c8f4d238169
sajjad akbari <seokarid@gmail.com>
2025-03-17 14:13:47 +0330

    Update functions.php

--- Seokar/functions.php
+++ Seokar/functions.php
@@ -1,9 +1,10 @@
 function seokar_enqueue_styles() {
-    wp_enqueue_style('seokar-style', get_template_directory_uri() . '/assets/css/style.css');
-    
-    // پشتیبانی از زبان‌های راست‌چین
+    // بارگذاری استایل اصلی
+    wp_enqueue_style('seokar-style', get_template_directory_uri() . '/assets/css/style.css', [], '1.0.0');
+
+    // بارگذاری استایل RTL فقط برای زبان‌های راست‌چین
     if (is_rtl()) {
-        wp_enqueue_style('seokar-rtl', get_template_directory_uri() . '/assets/css/rtl.css');
+        wp_enqueue_style('seokar-rtl', get_template_directory_uri() . '/assets/css/rtl.css', ['seokar-style'], '1.0.0');
     }
 }
 add_action('wp_enqueue_scripts', 'seokar_enqueue_styles');
