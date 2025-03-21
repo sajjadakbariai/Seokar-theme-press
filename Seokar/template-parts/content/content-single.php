9916be624250bb0e306c4a280667e83fa0645c71
sajjad akbari <seokarid@gmail.com>
2025-03-21 04:10:29 +0330

    Update content-single.php

--- Seokar/template-parts/content/content-single.php
+++ Seokar/template-parts/content/content-single.php
@@ -4,3 +4,9 @@ $likes = get_post_meta($post_id, 'seokar_likes', true);
 $likes = $likes ? $likes : 0;
 ?>
 <button class="like-button" data-postid="<?php echo $post_id; ?>">❤️ <?php echo $likes; ?></button>
+<?php
+$custom_field_value = seokar_get_custom_field(get_the_ID());
+if (!empty($custom_field_value)) {
+    echo '<p class="custom-field">📌 ' . esc_html($custom_field_value) . '</p>';
+}
+?>
