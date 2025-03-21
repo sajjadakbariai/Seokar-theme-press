b6b2aaf68a3cc99eeb6452176c6a82e9057a2d95
sajjad akbari <seokarid@gmail.com>
2025-03-21 06:07:07 +0330

    Update autoload.php

--- Seokar/inc/autoload.php
+++ Seokar/inc/autoload.php
@@ -3,18 +3,25 @@
  * Autoloader for Seokar Theme
  */
 
-spl_autoload_register( function ( $class ) {
+defined('ABSPATH') || exit; // جلوگیری از دسترسی مستقیم
+
+spl_autoload_register(function ($class) {
     $prefix = 'Seokar\\';
     $base_dir = __DIR__ . '/';
 
-    if ( strpos( $class, $prefix ) !== 0 ) {
+    // بررسی اینکه کلاس متعلق به فضای نام Seokar است
+    if (strpos($class, $prefix) !== 0) {
         return;
     }
 
-    $relative_class = substr( $class, strlen( $prefix ) );
-    $file = $base_dir . str_replace( '\\', '/', $relative_class ) . '.php';
+    // تبدیل کلاس به مسیر فایل
+    $relative_class = substr($class, strlen($prefix));
+    $file = $base_dir . str_replace(['\\', '_'], '/', $relative_class) . '.php';
 
-    if ( file_exists( $file ) ) {
+    // بررسی وجود فایل و بارگذاری آن
+    if (file_exists($file)) {
         require_once $file;
+    } else {
+        error_log("[Autoload Error] فایل یافت نشد: " . $file);
     }
 });
