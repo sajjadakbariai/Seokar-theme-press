d4c8ffda74b04f7e6936613c04c0bbae2e34e5ed
sajjad akbari <seokarid@gmail.com>
2025-03-21 04:17:17 +0330

    Create contact-page.php

---
+++ Seokar/template-parts/content/contact-page.php
@@ -0,0 +1,6 @@
+<form id="contact-form">
+    <input type="text" name="name" placeholder="نام شما" required>
+    <input type="email" name="email" placeholder="ایمیل شما" required>
+    <textarea name="message" placeholder="پیام شما" required></textarea>
+    <button type="submit">ارسال پیام</button>
+</form>
